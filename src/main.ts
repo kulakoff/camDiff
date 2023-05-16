5e1c81444b50a5f518dbe194e00c1701357a80bc
Anton Kulakov <anton.kulakoff@ya.ru>
2023-05-16 23:51:25 +0300

    feat: disabled cors for testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {cors: false});
+  app.enableCors({credentials: false, origin: false})
+  await app.listen(process.env.APP_PORT || 4002);
 }
 bootstrap();
