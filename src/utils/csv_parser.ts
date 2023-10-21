7fe52291825371b8c3b425bd74be476312d1dcc6
Anton Kulakov <anton.kulakoff@ya.ru>
2023-10-21 23:54:52 +0300

    refactor csv parser

--- src/utils/csv_parser.ts
+++ src/utils/csv_parser.ts
@@ -1,15 +1,27 @@
 import * as fs from 'fs';
 import csvParser from 'csv-parser';
 
+/**
+ * Represents data structure for a streaming source.
+ * @typedef {Object} StreamData
+ * @property {string} url - The URL of the streaming source.
+ * @property {string} client_id - The client ID associated with the streaming source.
+ */
+
 interface StreamData {
   url: string;
   client_id: string;
 }
 
 const streamList: StreamData[] = [];
-const csvFilePath = './cameras.csv';
-const jsonFilePath = './stream-list.json';
+const csvFilePath: string = './cameras.csv';
+const jsonFilePath: string = './stream-list.json';
 
+/**
+ * Reads a CSV file containing streaming data and converts it to JSON format.
+ * @param {string} csvFilePath - The path of the CSV file.
+ * @param {string} jsonFilePath - The path of the JSON file to write the result.
+ */
 fs.createReadStream(csvFilePath)
   .pipe(csvParser({ separator: ';' }))
   .on('data', (row:any) => {
